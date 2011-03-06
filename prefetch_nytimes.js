9ff6ed48bf8622a1f9acee2cc579213cf68ba0a5
Chris Bentzel <chris@bentzel.net>
2011-03-05 22:16:12 -0500

    Lots more areas for nytimes

--- prefetch_nytimes.js
+++ prefetch_nytimes.js
@@ -1,4 +1,17 @@
-function addPreloadHook(story) {
+function addPreloadToElement(elem, url) {
+    elem.onmouseover = function() {
+        // Would be good to only do this when over for a bit.
+        if (!elem.prefetchLink) {
+            var prefetchLink = document.createElement("link");
+            prefetchLink.setAttribute("rel", "prefetch");
+            prefetchLink.setAttribute("href", url);
+            elem.appendChild(prefetchLink);
+            elem.prefetchLink = prefetchLink;
+        }
+    }
+}
+
+function addPreloadToStory(story) {
     var anchor = story.getElementsByTagName("a")[0];
     if (!anchor) {
         return;
@@ -7,19 +20,63 @@ function addPreloadHook(story) {
     if (!dest_url) {
         return;
     }
-    story.onmouseover = function() {
-        if (!story.prefetchLink) {
-            var prefetchLink = document.createElement("link");
-            prefetchLink.setAttribute("rel", "prefetch");
-            prefetchLink.setAttribute("href", dest_url);
-            story.appendChild(prefetchLink);
-            story.prefetchLink = prefetchLink;
-        }
+    addPreloadToElement(story, dest_url);
+}
+
+function preloadStories() {
+    var stories = document.getElementsByClassName("story");
+    var story_length = stories.length;
+    for (var i = 0; i < story_length; ++i) {
+        addPreloadToStory(stories[i]);
     }
 }
 
-var stories = document.getElementsByClassName("story");
-var story_length = stories.length;
-for (var i = 0; i < story_length; ++i) {
-    addPreloadHook(stories[i]);
+function addPreloadToAnchor(anchor) {
+    addPreloadToElement(anchor, anchor.getAttribute("href"));
 }
+
+function addPreloadToAnchors(anchors) {
+    var anchor_length = anchors.length;
+    for (var i = 0; i < anchor_length; ++i) {
+        addPreloadToAnchor(anchors[i]);
+    }
+}
+
+function preloadNext() {
+    var nextLinks = document.getElementsByClassName("next");
+    addPreloadToAnchors(nextLinks);
+}
+
+function preloadPrevious() {
+    var prevLinks = document.getElementsByClassName("previous");
+    addPreloadToAnchors(prevLinks);
+}
+
+function addPreloadToTabEntry(tab_entry) {
+    var a = tab_entry.getElementsByTagName("a");
+    if (a.length != 1) {
+        return;
+    }
+    addPreloadToElement(tab_entry, a[0].getAttribute("href"));
+}
+
+function addPreloadToTab(tab) {
+    var entries = tab.getElementsByTagName("li");
+    var entry_length = entries.length;
+    for (var i = 0; i < entry_length; ++i) {
+        addPreloadToTabEntry(entries[i]);
+    }
+}
+
+function preloadTabs() {
+    var tabs = document.getElementsByClassName("tabs");
+    var tab_length = tabs.length;
+    for (var i = 0; i < tab_length; ++i) {
+        addPreloadToTab(tabs[i]);
+    }
+}
+
+preloadStories();
+preloadNext();
+preloadPrevious();
+preloadTabs();
