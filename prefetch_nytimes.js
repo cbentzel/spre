e095f8d286f1b34f5842b9aa33387a4c1b0f4137
Chris Bentzel <chris@bentzel.net>
2011-03-06 12:25:05 -0500

    Only trigger prerender after 300ms of hovering

--- prefetch_nytimes.js
+++ prefetch_nytimes.js
@@ -1,21 +1,32 @@
 function addPreloadToElement(elem, url) {
-    var removePrefetchLink = function(elem) {
+    var removePrefetchLink = function() {
         if (elem.prefetchLink) {
             elem.removeChild(elem.prefetchLink);
             elem.prefetchLink = undefined;
         }
     }
-    var jquery_obj = $(elem);
-    jquery_obj.mouseenter(function() {
-        removePrefetchLink(elem);
+    var createPrefetchLink = function() {
+        removePrefetchLink();
         var prefetchLink = document.createElement("link");
         prefetchLink.setAttribute("rel", "prefetch");
         prefetchLink.setAttribute("href", url);
         elem.appendChild(prefetchLink);
         elem.prefetchLink = prefetchLink;
+    }
+    var hover_delay_milliseconds = 300;
+    var jquery_obj = $(elem);
+    jquery_obj.mouseenter(function() {
+        elem.delayed_prefetch = setTimeout(function() {
+            elem.delayed_prefetch = undefined;
+            createPrefetchLink();
+        }, hover_delay_milliseconds);        
     });
     jquery_obj.mouseleave(function() {
-        removePrefetchLink(elem);
+        if (elem.delayed_prefetch) {
+            clearTimeout(elem.delayed_prefetch);
+            elem.delayed_prefetch = undefined;
+        }
+        removePrefetchLink();
     });
 }
 
